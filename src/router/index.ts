aebbc8bb8873afe76d212e3b1b2f02f1444d827a
ugurtortum7 <ugurtortum44@gmail.com>
2025-11-17 01:00:45 +0300

    added footer page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import HeaderPage from '@/views/HeaderPage.vue'
+import FooterPage from '@/views/FooterPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'HomePage', component: HomePage },
     { path: '/header', name: 'HeaderPage', component: HeaderPage },
+    { path: '/footer', name: 'FooterPage', component: FooterPage },
   ],
 })
 
---
+++ src/views/FooterPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <SiteFooter />
+  </div>
+</template>
+
+<script setup>
+import SiteFooter from '../components/SiteFooter.vue'
+</script>
+
+<style scoped>
+/* simple page styles */
+</style>
