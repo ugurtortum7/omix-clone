ff503f3015b9e4ca2ba2c09e79e2f06852cd4f9c
ugurtortum7 <ugurtortum44@gmail.com>
2025-11-17 01:07:52 +0300

    added purchase page

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import SignInPage from '@/views/SignInPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
 import ProductPage from '@/views/ProductPage.vue'
 import TechFeaturesPage from '@/views/TechFeaturesPage.vue'
+import PurchasePage from '@/views/PurchasePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,7 @@ const router = createRouter({
     { path: '/register', name: 'RegisterPage', component: RegisterPage },
     { path: '/product', name: 'ProductPage', component: ProductPage },
     { path: '/tech-features', name: 'TechFeaturesPage', component: TechFeaturesPage },
+    { path: '/purchase', name: 'PurchasePage', component: PurchasePage },
   ],
 })
 
