005c92efdf88fc9b63e67d03995aa6f09577c048
ugurtortum7 <ugurtortum44@gmail.com>
2025-11-17 01:06:35 +0300

    added tech-features page

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import FooterPage from '@/views/FooterPage.vue'
 import SignInPage from '@/views/SignInPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
 import ProductPage from '@/views/ProductPage.vue'
+import TechFeaturesPage from '@/views/TechFeaturesPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,7 @@ const router = createRouter({
     { path: '/sign-in', name: 'SignInPage', component: SignInPage },
     { path: '/register', name: 'RegisterPage', component: RegisterPage },
     { path: '/product', name: 'ProductPage', component: ProductPage },
+    { path: '/tech-features', name: 'TechFeaturesPage', component: TechFeaturesPage },
   ],
 })
 
