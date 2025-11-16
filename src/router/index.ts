216a02e30605307fd3f32a6caea0244ec09ac716
ugurtortum7 <ugurtortum44@gmail.com>
2025-11-17 01:02:04 +0300

    added sign-in page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import HeaderPage from '@/views/HeaderPage.vue'
 import FooterPage from '@/views/FooterPage.vue'
+import SignInPage from '@/views/SignInPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
     { path: '/', name: 'HomePage', component: HomePage },
     { path: '/header', name: 'HeaderPage', component: HeaderPage },
     { path: '/footer', name: 'FooterPage', component: FooterPage },
+    { path: '/sign-in', name: 'SignInPage', component: SignInPage },
   ],
 })
 
