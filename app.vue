37af164fabb7bc680d7fb3ac19d4291bc623e95d
ugurtortum7 <ugurtortum44@gmail.com>
2026-01-22 04:01:51 +0300

    feat: Nuxt.js ana entry point eklendi
    - app.vue uygulamanın root componenti
    - NuxtPage ve NuxtLayout desteği

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAuthStore } from '~/stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  // Firebase auth state listener'ı başlat
+  authStore.initAuthListener()
+})
+</script>
